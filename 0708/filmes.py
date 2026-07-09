2fd976961bb2f847bc3708027be68773be56155f
matheusbuenoeisenhardt-debug <ma.bueno.eisenhardt@gmail.com>
2026-07-08 22:23:45 -0300

    0708

--- 0708/filmes.py
+++ 0708/filmes.py
@@ -9,10 +9,29 @@ print("3 - filmes de um diretor especifico")
 print("4 - filmes de um genero especifico")
 print("5 - media de duração dos filmes")
 print("6 - sair")
-escolha = input("qual opção deseja escolher: ")
+escolha = input("qual opção deseja escolher: ").strip()
 
 if escolha == 0:
+    adicionar_filme()
 
 elif escolha == 1:
+    contar_filmes()
 
-elif
+elif escolha == 2:
+    info_por_titulo()
+
+elif escolha == 3:
+    filmes_por_diretor()
+
+elif escolha == 4:
+    filmes_por_genero()
+
+elif escolha == 5:
+    media_duração()
+
+elif escolha == 6:
+    print("encerrando sistema")
+    break 
+
+else:
+    print("opção invalida, digite novamente.")
