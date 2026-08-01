5af90362acce96b0aee3a9e5187f856cbe6e6abb
matheusbuenoeisenhardt-debug <ma.bueno.eisenhardt@gmail.com>
2026-08-01 02:01:32 -0300

    0801

--- tkinter0731/pack_em_ação.py
+++ tkinter0731/pack_em_ação.py
@@ -1,12 +1,17 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title("SENAI - Desenvolvimento de Sisteas")
+root.title("SENAI - Desenvolvimento de Sistemas")
 root.geometry("340x100")
 
-tk.Button(root,text="Top Button!").pack()
-tk.Label(root, text"Hello, Left!").pack(side="left")
-tk.Label(root, text"Hello, Right!").pack(side="right")
-tk.Checkbutton(root, text="Uma opção na parte inferior!")
-    .pack(side=tk.BOTTOM)
+# Botão no topo
+tk.Button(root, text="Top Button!").pack()
+
+# Labels alinhadas à esquerda e à direita
+tk.Label(root, text="Hello, Left!").pack(side="left")
+tk.Label(root, text="Hello, Right!").pack(side="right")
+
+# Checkbutton na parte inferior
+tk.Checkbutton(root, text="Uma opção na parte inferior!").pack(side=tk.BOTTOM)
+
 root.mainloop()
